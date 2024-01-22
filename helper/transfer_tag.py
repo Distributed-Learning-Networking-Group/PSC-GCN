a93e7f7566de98420805b3ecedbb83aeb07d5a7f
sl-1314 <82856253+sl-1314@users.noreply.github.com>
2024-01-22 10:13:07 +0800

    Add files via upload

---
+++ helper/transfer_tag.py
@@ -0,0 +1,13 @@
+class TransferTag:
+    SELECT1 = 0
+    SELECT2 = 1
+    SAMPLE1 = 5
+    SAMPLE2 = 6
+    CACHE_INIT1 = 13
+    CACHE_INIT2 = 14
+    CACHE_USE1 = 15
+    CACHE_USE2 = 16
+    CACHE_UPDATE1 = 17
+    CACHE_UPDATE2 = 18
+    CACHE_UPDATE3 = 19
+    FEAT_BEGIN = 20
